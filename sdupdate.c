ecae392e2181c4911b48f3d170cbc58d0bd8598e
Danny Abukalam <dannyabukalam@codethink.co.uk>
2011-11-21 14:27:22 +0000

    Fixed bugs.

--- sdupdate.c
+++ sdupdate.c
@@ -1,57 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (char argc, const char **argv) {
-	const char* srce = argv[1];
-	const char* dest = argv[2];
-
-	if (srce == NULL) {
-		printf("Error: No Source File Specified\n");
-		return EXIT_FAILURE;
-	}
-	if (dest == NULL) {
-		printf("Error: No Destination File Specified\n");
-		return EXIT_FAILURE;
-	}
-
-	printf("Source: %s\n", srce);
-	printf("Destination: %s\n", dest);
-
-	int a = copy(srce, dest);
-	if (a!=1) {
-		return EXIT_FAILURE;
-	}
-
-	if (a==1) 
-		printf("File Copied Successfully.\n");
+int copy (const char* srce, const char* dest) {
 	
+	FILE *srcfile = fopen (srce,"rb");
+	if (srcfile==NULL) {
+		fprintf(stderr,"File could not be read.\n");
+		return 1;
+	}
+
+	FILE *destfile = fopen (dest,"wb");
+	if (destfile == NULL) {
+		fprintf(stderr,"File write error.\n");
+		return 1;
+	}
+
+	int tmpch;
+	while ((tmpch = fgetc(srcfile)) !=EOF)
+		putc(tmpch, destfile);
+
+	fflush(destfile);
+
+	if (ferror(destfile)) {
+		fprintf(stderr,"File write error.\n");
+		return 1;
+	}
+
+	fclose(destfile);
 	return 0;
 }
 
-int copy (const char* srce, char* dest) {
-	if (file_exists(dest)) {
-		printf("Error: Destination File Exists\n");
-		return 0;
-	}
-	FILE *srcfile = fopen (srce,"r");
-	if (srcfile==NULL) {
-		printf("Error: File Could not be read\n");
-		return 0;
-	}
-	FILE *destfile = fopen (dest,"w");
-	char tmpch;
-	while (!feof(srcfile)) {
-		tmpch = fgetc(srcfile);
-		putc(tmpch, destfile);
-	}
-	fclose(destfile);
-	return 1;
-}
+int main (char argc, const char **argv) {
+	
+	const char* srce = argv[1];
+	const char* dest = argv[2];
 
-int file_exists(char* path) {
-	FILE* file;
-    if (file = fopen(path,"r"))
-		return 1;
-	else
-		return 0;
+	if (argc != 3) {
+		fprintf(stderr,"Invalid number of arguments.\n");
+		return EXIT_FAILURE;
+	}
+
+	int failure = copy(srce, dest);
+	if (failure) {
+		fprintf(stderr,"Copy Failure"); 
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
