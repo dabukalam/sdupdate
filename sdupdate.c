78be50775e56758c89d6f16e8aa62d88650a47ad
Danny Abukalam <dannyabukalam@codethink.co.uk>
2011-11-21 16:26:05 +0000

    Fixed Error Reporting

--- sdupdate.c
+++ sdupdate.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <stdlib.h>
 
 int copy (const char* srce, const char* dest) {
 	
 	FILE *srcfile = fopen (srce,"rb");
 	if (srcfile==NULL) {
-		fprintf(stderr,"File could not be read.\n");
+		fprintf(stderr, "%s: file could not be read: %d: %s\n", srce, errno, strerror(errno));
 		return 1;
 	}
 
 	FILE *destfile = fopen (dest,"wb");
 	if (destfile == NULL) {
-		fprintf(stderr,"File write error.\n");
+		fprintf(stderr, "%s: file write error: %d: %s\n", dest, errno, strerror(errno));
 		return 1;
 	}
 
@@ -19,30 +21,36 @@ int copy (const char* srce, const char* dest) {
 	while ((tmpch = fgetc(srcfile)) !=EOF)
 		putc(tmpch, destfile);
 
-	fflush(destfile);
-
-	if (ferror(destfile)) {
-		fprintf(stderr,"File write error.\n");
+	if (fflush(destfile)) {
+		fprintf(stderr,"error while emptying buffer: %d: %s\n", errno, strerror(errno));
 		return 1;
 	}
 
-	fclose(destfile);
+	if (ferror(destfile)) {
+		fprintf(stderr,"%s: file write error: %d: %s\n", dest, errno, strerror(errno));
+		return 1;
+	}
+
+	if (fclose(destfile)) {
+		fprintf(stderr,"%s: error closing file: %d: %s\n", dest, errno, strerror(errno));
+		return 1;
+	}
 	return 0;
 }
 
 int main (char argc, const char **argv) {
 	
+	if (argc != 3) {
+		fprintf(stderr,"invalid number of arguments: %d: %s\n", errno, strerror(errno));
+		return EXIT_FAILURE;
+	}
+	
 	const char* srce = argv[1];
 	const char* dest = argv[2];
 
-	if (argc != 3) {
-		fprintf(stderr,"Invalid number of arguments.\n");
-		return EXIT_FAILURE;
-	}
-
 	int failure = copy(srce, dest);
 	if (failure) {
-		fprintf(stderr,"Copy Failure"); 
+		fprintf(stderr,"copy failure: %d: %s\n", errno, strerror(errno));
 		return EXIT_FAILURE;
 	}
 
