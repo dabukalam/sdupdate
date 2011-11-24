1e7150d5c104421cf44a5778468f9e769676ea9b
Danny Abukalam <dannyabukalam@codethink.co.uk>
2011-11-24 12:09:07 +0000

    Replaced tabspace with whitespace Extra error checking

--- sdupdate.c
+++ sdupdate.c
@@ -4,64 +4,82 @@
 #include <stdlib.h>
 
 int copy (const char* srce, const char* dest) {
-	
-	FILE *srcfile = fopen (srce,"rb");
-	if (srcfile==NULL) {
-		fprintf(stderr, "%s: file could not be read: %d: %s\n", srce, errno, strerror(errno));
-		return 1;
-	}
+    
+    FILE *srcfile = fopen (srce,"rb");
+    if (srcfile==NULL) {
+        fprintf(stderr, "%s: file could not be read: %d: %s\n", srce, errno,
+        strerror(errno));
+        
+        return 1;
+    }
 
-	FILE *destfile = fopen (dest,"wb");
-	if (destfile == NULL) {
-		fprintf(stderr, "%s: file write error: %d: %s\n", dest, errno, strerror(errno));
-		return 1;
-	}
+    FILE *destfile = fopen (dest,"wb");
+    if (destfile == NULL) {
+        fprintf(stderr, "%s: file write error: %d: %s\n", dest, errno,
+        strerror(errno));
+        
+        return 1;
+    }
 
-	int blocksize = 1;
-	char tmpch[100];
-	int count = 100;
-	int j = count;
+    int blocksize = 1;
+    char tmpch[100];
+    int count, lastw; 
+    lastw = count = sizeof(tmpch);
 
-	while (j == count) {
-		j = fread (&tmpch, blocksize, count, srcfile);
-		if (j != count) 
-			fwrite(tmpch, blocksize, j, destfile);
-		else
-			fwrite(tmpch, blocksize, count, destfile);
-	}
+    do {
+        lastw = fread (tmpch, blocksize, count, srcfile);
+        fwrite(tmpch, blocksize, lastw, destfile);
+    } while (lastw > 0);
 
-	if (fflush(destfile)) {
-		fprintf(stderr,"error while emptying buffer: %d: %s\n", errno, strerror(errno));
-		return 1;
-	}
+    if (fflush(destfile)) {
+        fprintf(stderr,"error while emptying buffer: %d: %s\n", errno,
+        strerror(errno));
+        
+        return 1;
+    }
 
-	if (ferror(destfile)) {
-		fprintf(stderr,"%s: file write error: %d: %s\n", dest, errno, strerror(errno));
-		return 1;
-	}
+    if (ferror(srcfile)) {
+        fprintf(stderr,"%s: file read error: %d: %s\n", srce, errno,
+        strerror(errno));
+        
+        return 1;
+    }
 
-	if (fclose(destfile)) {
-		fprintf(stderr,"%s: error closing file: %d: %s\n", dest, errno, strerror(errno));
-		return 1;
-	}
-	return 0;
+    if (ferror(destfile)) {
+        fprintf(stderr,"%s: file write error: %d: %s\n", dest, errno,
+        strerror(errno));
+        
+        return 1;
+    }
+
+    if (fclose(destfile)) {
+        fprintf(stderr,"%s: error closing file: %d: %s\n", dest, errno,
+        strerror(errno));
+        
+        return 1;
+    }
+    
+    return 0;
 }
 
 int main (int argc, const char **argv) {
-	
-	if (argc != 3) {
-		fprintf(stderr,"invalid number of arguments: %d: %s\n", errno, strerror(errno));
-		return EXIT_FAILURE;
-	}
-	
-	const char* srce = argv[1];
-	const char* dest = argv[2];
+    
+    if (argc != 3) {
+        fprintf(stderr,"invalid number of arguments: %d: %s\n", errno,
+        strerror(errno));
+        
+        return EXIT_FAILURE;
+    }
+    
+    const char* srce = argv[1];
+    const char* dest = argv[2];
 
-	int failure = copy(srce, dest);
-	if (failure) {
-		fprintf(stderr,"copy failure: %d: %s\n", errno, strerror(errno));
-		return EXIT_FAILURE;
-	}
+    int failure = copy(srce, dest);
+    if (failure) {
+        fprintf(stderr,"copy failure: %d: %s\n", errno, strerror(errno));
+        
+        return EXIT_FAILURE;
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;    
 }
